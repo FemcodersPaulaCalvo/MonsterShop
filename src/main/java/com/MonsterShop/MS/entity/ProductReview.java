bd5822101b586e818a36da841c4f266c13a21474
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-13 16:53:53 +0200

    feat: ProductReview --> atributes, constructor, getters

---
+++ src/main/java/com/MonsterShop/MS/entity/ProductReview.java
@@ -0,0 +1,36 @@
+package com.MonsterShop.MS.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product_review")
+public class ProductReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_product")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "id_review")
+    private Review review;
+
+    public ProductReview() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+}
