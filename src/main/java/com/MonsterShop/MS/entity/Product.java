9fc7352244e488a82bec807a4b32c6d2ae59c581
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-13 16:45:22 +0200

    feat: Product --> atributes, constructor, getters , setters

--- src/main/java/com/MonsterShop/MS/entity/Product.java
+++ src/main/java/com/MonsterShop/MS/entity/Product.java
@@ -1,4 +1,89 @@
 package com.MonsterShop.MS.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    String name;
+
+    double price;
+
+    String imageUrl;
+
+    double rating;
+
+    int reviewCount;
+
+    boolean featured;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductReview> productReviews = new ArrayList<>();
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getReviewCount() {
+        return reviewCount;
+    }
+
+    public boolean isFeatured() {
+        return featured;
+    }
+
+    public List<ProductReview> getProductReviews() {
+        return productReviews;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setReviewCount(int reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public void setFeatured(boolean featured) {
+        this.featured = featured;
+    }
 }
