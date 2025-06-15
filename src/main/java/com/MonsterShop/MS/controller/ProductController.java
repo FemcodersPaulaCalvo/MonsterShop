3060606e123b0fa703c7523d5c5ebf6b75fc683d
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-15 19:22:33 +0200

    feat: ProductController --> add method getProductById

--- src/main/java/com/MonsterShop/MS/controller/ProductController.java
+++ src/main/java/com/MonsterShop/MS/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.MonsterShop.MS.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class ProductController {
     public ResponseEntity<List<ResponseProductDto>> getAllProductsList(){
         List<ResponseProductDto> products = PRODUCT_SERVICE.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseProductDto> getProductById(@PathVariable Long id){
+        ResponseProductDto product = PRODUCT_SERVICE.getProductById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
