326a558d426f89ec6d77c9cc2bc65b81d2b47bbe
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-15 17:35:25 +0200

    feat: dto/product --> add RequestProductDto, ResponseProductDto, MapperProductDto

--- src/main/java/com/MonsterShop/MS/dto/product/ResponseProductDto.java
+++ src/main/java/com/MonsterShop/MS/dto/product/ResponseProductDto.java
@@ -1,4 +1,12 @@
 package com.MonsterShop.MS.dto.product;
 
-public record ResponseProductDto() {
+public record ResponseProductDto(
+        Long id,
+        String name,
+        double price,
+        String imageUrl,
+        double rating,
+        int reviewCount,
+        boolean featured
+) {
 }
