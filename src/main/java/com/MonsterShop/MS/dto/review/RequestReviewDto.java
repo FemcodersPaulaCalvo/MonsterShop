87d455af25c12735d1d1e2ab8213b4d2f9595753
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-16 09:54:02 +0200

    feat: dto/review --> RequestProductDto, RescponseProductDto, MapperReviewDto

--- src/main/java/com/MonsterShop/MS/dto/review/RequestReviewDto.java
+++ src/main/java/com/MonsterShop/MS/dto/review/RequestReviewDto.java
@@ -1,4 +1,20 @@
 package com.MonsterShop.MS.dto.review;
 
-public record RequestReviewDto() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+public record RequestReviewDto(
+        @NotBlank(message = "Username is required")
+        @Size(min = 2, max = 50, message = "Username must contain min 2 and max 50 characters")
+        String username,
+
+        @PositiveOrZero(message = "The number entered must be positive or zero")
+        @NotBlank(message = "Rating is required")
+        double rating,
+
+        @NotBlank(message = "The body is required")
+        @Size(min = 3, message = "Name must contain min 3")
+        String body
+) {
 }
