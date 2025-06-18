de7e0159799a1b9b60538798f01eae44e42740f2
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-18 10:45:27 +0200

    feat: add description dto/product, entity/Product, service/ProductService

--- src/main/java/com/MonsterShop/MS/repository/ProductRepository.java
+++ src/main/java/com/MonsterShop/MS/repository/ProductRepository.java
@@ -2,8 +2,6 @@ package com.MonsterShop.MS.repository;
 
 import com.MonsterShop.MS.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.expression.spel.ast.OpAnd;
-
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
